94b2767c2eb451dbb2ddf8925ee4075c9e831a2b
tess leroy <tess.leroy@epfedu.fr>
2025-10-24 12:51:19 +0200

    création d'un nouveau projet versionné

---
+++ TP0_helloworld/src/tp0_helloworld/TP0_helloworld.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_helloworld;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tessl
+ */
+public class TP0_helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {System.out.println( " Bonjour " ) ;
+   String prenom; 
+Scanner sc ; 
+sc = new Scanner (System.in) ; 
+System.out.println("Quel est votre prénom ?"); 
+prenom = sc.nextLine(); 
+        // TODO code application logic here
+    }
+    
+}
