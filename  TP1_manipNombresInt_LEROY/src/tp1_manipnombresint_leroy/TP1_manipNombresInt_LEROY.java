c462e3d10497dcfe6bcdbefa87e64aeaf4f61a9e
tess leroy <tess.leroy@epfedu.fr>
2025-11-03 14:34:13 +0100

    création d'un nouveau projet versionné

---  TP1_manipNombresInt_LEROY/src/tp1_manipnombresint_leroy/TP1_manipNombresInt_LEROY.java
+++  TP1_manipNombresInt_LEROY/src/tp1_manipnombresint_leroy/TP1_manipNombresInt_LEROY.java
@@ -4,6 +4,8 @@
  */
 package tp1_manipnombresint_leroy;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tessl
@@ -13,8 +15,30 @@ public class TP1_manipNombresInt_LEROY {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) {try (Scanner sc = new Scanner(System.in)) {
+        System.out.print("Entrez le premier entier : ");
+        int a = sc.nextInt();
+        
+        System.out.print("Entrez le second entier : ");
+        int b = sc.nextInt();
+        
+        
+        System.out.println("Vous avez saisi : " + a + " et " + b);
+        
+  
+        System.out.println("Somme : " + (a + b));
+        System.out.println("Différence : " + (a - b));
+        System.out.println("Produit : " + (a * b));
+        
+      
+        System.out.println("Quotient entier : " + (a / b));
+        System.out.println("Reste de la division euclidienne : " + (a % b));
+
+        System.out.println("Quotient exact : " + ((double) a / b));
+        }
     }
-    
 }
+        
+    
+  
+
