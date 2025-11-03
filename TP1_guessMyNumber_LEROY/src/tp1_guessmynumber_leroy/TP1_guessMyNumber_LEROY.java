d1c7008b024af5951b8e686a9d6541ecabed0224
tess leroy <tess.leroy@epfedu.fr>
2025-11-03 15:01:36 +0100

    création d'un nouveau projet versionné

---
+++ TP1_guessMyNumber_LEROY/src/tp1_guessmynumber_leroy/TP1_guessMyNumber_LEROY.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_guessmynumber_leroy;
+
+/**
+ *
+ * @author tessl
+ */
+public class TP1_guessMyNumber_LEROY {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
