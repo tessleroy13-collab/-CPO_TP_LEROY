e51c9e5b2f7a06a791bfb31c74dbeb37dcf6625f
tess leroy <tess.leroy@epfedu.fr>
2025-11-12 10:34:45 +0100

    création d'un nouveau projet versionné

--- TP2_Bieres_LEROY/src/tp2_bieres_leroy/BouteilleBiere.java
+++ TP2_Bieres_LEROY/src/tp2_bieres_leroy/BouteilleBiere.java
@@ -8,6 +8,50 @@ package tp2_bieres_leroy;
  *
  * @author tessl
  */
-public class BouteilleBiere {
-    
+public class BouteilleBiere {  
+    String nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;
+   
+ public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+        nom = unNom;
+        degreAlcool = unDegre;
+        brasserie = uneBrasserie;
+        ouverte = false;
+    }
+
+    public void lireEtiquette() {
+        System.out.println("Bouteille de " + nom + " (" + degreAlcool + " degres)\nBrasserie : " + brasserie);
+    }
+   
+    public boolean Decapsuler() {
+        if (!ouverte) {
+            ouverte = true;
+            return true;
+        } else {
+            System.out.println("Erreur : biere déjà ouverte");
+            return false;
+        }    
 }
+   
+
+
+@Override
+    public String toString() {
+        String chaine_a_retourner = nom + " (" + degreAlcool + " degrés) - Ouverte ? ";
+        if (ouverte) {
+            chaine_a_retourner += "oui";
+        } else {
+            chaine_a_retourner += "non";
+        }
+        return chaine_a_retourner;
+    }
+
+    void decapsuler() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
+
+    
+
--- TP2_Bieres_LEROY/src/tp2_bieres_leroy/TP2_Bieres_LEROY.java
+++ TP2_Bieres_LEROY/src/tp2_bieres_leroy/TP2_Bieres_LEROY.java
@@ -14,7 +14,34 @@ public class TP2_Bieres_LEROY {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0, "Dubuisson");
+        uneBiere.lireEtiquette();
+
+        BouteilleBiere deuxBiere = new BouteilleBiere("Leffe", 6.6, "Abbaye de Leffe");
+        deuxBiere.lireEtiquette();
+        deuxBiere.Decapsuler();  
+        deuxBiere.Decapsuler();
+       
+        BouteilleBiere biere3 = new BouteilleBiere("Heineken", 5.0, "Heineken International");
+        biere3.lireEtiquette();
+        biere3.Decapsuler();
+     
+        BouteilleBiere biere4 = new BouteilleBiere("Chimay Bleue", 9.0, "Abbaye de Scourmont");
+        biere4.lireEtiquette();
+       
+        BouteilleBiere biere5 = new BouteilleBiere ("Karmeliet Triple", 8.4, "Bosteels");
+        biere5.lireEtiquette();
+        biere5.Decapsuler();
+       
+        System.out.println(uneBiere);
+        System.out.println(deuxBiere);
+        System.out.println(biere3);
+        System.out.println(biere4);
+        System.out.println(biere5);
+
     }
-    
+    }
 }
+   
+
