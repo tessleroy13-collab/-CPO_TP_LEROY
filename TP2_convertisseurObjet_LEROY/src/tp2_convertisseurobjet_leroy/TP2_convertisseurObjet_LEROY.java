33b73e5935e7125596b7f4d9fe645cde8da08eae
tess leroy <tess.leroy@epfedu.fr>
2025-11-12 10:49:27 +0100

    création d'un nouveau projet versionné

--- TP2_convertisseurObjet_LEROY/src/tp2_convertisseurobjet_leroy/TP2_convertisseurObjet_LEROY.java
+++ TP2_convertisseurObjet_LEROY/src/tp2_convertisseurobjet_leroy/TP2_convertisseurObjet_LEROY.java
@@ -14,7 +14,25 @@ public class TP2_convertisseurObjet_LEROY {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Convertisseur conv1 = new Convertisseur();
+        Convertisseur conv2 = new Convertisseur();
+
+        double f = conv1.celsiusToFahrenheit(0);     // 0°C -> 32°F
+        double k = conv1.celsiusToKelvin(25);        // 25°C -> 298.15K
+        double c = conv2.fahrenheitToCelsius(212);   // 212°F -> 100°C
+        double c2 = conv2.kelvinToCelsius(273.15);   // 273.15K -> 0°C
+
+        System.out.println("0°C -> " + f + "°F");
+        System.out.println("25°C -> " + k + "K");
+        System.out.println("212°F -> " + c + "°C");
+        System.out.println("273.15K -> " + c2 + "°C");
+
+        System.out.println("\nÉtat des convertisseurs :");
+        System.out.println("conv1 : " + conv1);
+        System.out.println("conv2 : " + conv2);
     }
-    
 }
+
+    
+    
+
