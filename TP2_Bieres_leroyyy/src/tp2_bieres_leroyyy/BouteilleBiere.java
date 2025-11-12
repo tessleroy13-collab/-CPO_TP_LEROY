8400ff82f878675b91f7edf189d743d5410c4be2
tess leroy <tess.leroy@epfedu.fr>
2025-11-12 10:04:00 +0100

    création d'un nouveau projet versionné

--- TP2_Bieres_leroyyy/src/tp2_bieres_leroyyy/BouteilleBiere.java
+++ TP2_Bieres_leroyyy/src/tp2_bieres_leroyyy/BouteilleBiere.java
@@ -8,6 +8,39 @@ package tp2_bieres_leroyyy;
  *
  * @author tessl
  */
-public class BouteilleBiere {
+public class BouteilleBiere {String nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;
+
     
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+        nom = unNom;
+        degreAlcool = unDegre;
+        brasserie = uneBrasserie;
+        ouverte = false;
+    }
+
+ 
+    public void lireEtiquette() {
+        System.out.println("Bouteille de " + nom + " (" + degreAlcool + " degres)\nBrasserie : " + brasserie);
+    }
+
+   
+    public boolean decapsuler() {
+        if (!ouverte) {
+            ouverte = true;
+            return true;
+        } else {
+            System.out.println("Erreur : biere déjà ouverte");
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return nom + " (" + degreAlcool + " degrés) Ouverte ? " + (ouverte ? "oui" : "non");
+    }
 }
+    
+
